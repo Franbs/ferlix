295875e6f9c74176bc66033008b5ca31f6e0b8b5
Francesc Banzo Saavedra <12francescbs@gmail.com>
2021-12-03 20:46:52 +0100

    payment-form view created

---
+++ resources/views/user/payment-form.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Payment form</title>
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <a href="/" class="d-flex justify-content-center text-dark text-decoration-none">
+            <svg width="173" height="140">
+                <text class="text-align-center" x="0" y="100" fill="red" font-weight="bold" font-size="3em">FERLIX</text>
+            </svg>
+        </a>
+    </div>
+
+    <form>
+        <div class="container w-25 p-3">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon2">
+            </div>
+
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Card number" aria-label="Card number" aria-describedby="basic-addon2">
+            </div>
+
+            <div class="container p-0 mb-3">
+                <div class="row">
+                    <div class="col input-group">
+                        <input placeholder="Expiration date" class="textbox-n form-control mr-1" type="text" onfocus="(this.type='date')" id="date">
+                    </div>
+
+                    <div class="col">
+                        <i class="bi bi-calendar mt-1"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="input-group mb-5">
+                <input type="text" class="form-control" placeholder="CVV" aria-label="CVV" aria-describedby="basic-addon2">
+            </div>
+
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Dirección" aria-label="Direccion" aria-describedby="basic-addon2">
+                <input type="text" class="form-control" placeholder="Ciudad" aria-label="Ciudad" aria-describedby="basic-addon2">
+            </div>
+
+            <div class="input-group mb-5">
+                <input type="text" class="form-control" placeholder="Provincia" aria-label="Provincia" aria-describedby="basic-addon2">
+                <input type="text" class="form-control" placeholder="Código postal" aria-label="Codigo postal" aria-describedby="basic-addon2">
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <button type="submit" class="btn btn-primary justify-content-center">Submit</button>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\ViewServiceProvider;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,90 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+/* # */
+Route::get('/', function() {
+    return view("home");
+});
+Route::get('/billboard', function() {
+    return view("billboard");
+});
+Route::get('/bilboard?filter="{filter}"', function($filter) {
+    return view("billboard", compact($filter));
+});
+Route::get('/bilboard?search="{search}"', function($search) {
+    return view("bilboard", compact($search));    
+});
+Route::get('/login', function() {
+    return view("login-form");
+});
+Route::get('/login/loading', function() {
+    return view("login-form");
+});
+Route::get('/register', function() {
+    return view("register-form");
+});
+Route::get('/register/loading', function() {
+    return view("register-form");
+});
+
+/* USUARIO */
+
+Route::get('/payment', function() {
+    return view("user.payment-form");
+});
+Route::get('/payment/loading', function() {
+    return view("payment-form");
+});
+Route::get('/user', function() {
+    return view("user-form");
+});
+Route::get('/user/loading', function() {
+    return view("user-form");
+});
+Route::get('/stream', function() {
+    return view("stream");
+});
+Route::get('/stream/fav', function() {
+    return view("stream");
+});
+Route::get('/stream/next-episode', function() {
+    return view("stream");
+});
+Route::get('/favorites', function() {
+    return view("favorites");
+});
+Route::get('/favorites/fav', function() {
+    return view("favorites");
+});
+
+/* Administrador */
+Route::get('/user-list', function() {
+    return view("user-list");
+});
+Route::get('/user-list/info', function() {
+    return view("user-list-info");
+});
+Route::get('/user-list/info/block', function() {
+    return view("user-list-info");
+});
+Route::get('/user-list/info/unblock', function() {
+    return view("user-list-info");
+});
+Route::get('/user-list/info/allow', function() {
+    return view("user-list-info");
+});
+Route::get('/media-list', function() {
+    return view("media-list");
+});
+Route::get('/media-list/info', function() {
+    return view("media-list-info");
+});
+Route::get('/media-list/info/save', function() {
+    return view("media-list-info");
+});
+Route::get('/media-list/add', function() {
+    return view("media-list-info");
+});
+Route::get('/media-list/discard', function() {
+    return view("media-list-info");
 });
