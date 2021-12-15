c579adccda6043f358377a832bafad91e7714ae2
EduBS <2wiawedubs@gmail.com>
2021-12-15 18:36:47 +0100

    Home working with controller, model and view.

--- resources/views/billboard.blade.php
+++ resources/views/billboard.blade.php
@@ -3,11 +3,32 @@
 @section("page-title", "Home")
 
 @section("content")
-@foreach ($movies as $movie)
+{{-- @foreach ($movies as $movie)
 <div>
-    <img src="baseDatos" alt="">
-    <h2>{{$movie}}</h2>
+    <img src="{{$movie['image']}}" alt="">
+    <h2>{{$movie['title']}}</h2>
 </div>
-@endforeach
+@endforeach --}}
+
+<div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        
+        @foreach ($movies as $movie)
+        <div class="col">
+          <div class="card shadow-sm">
+            <img class="card-img-top" src="{{$movie["image"]}}" alt="">
+            <div class="card-body text-center">
+              <h5 class="card-title">{{$movie["title"]}}</h5>
+              <button class="btn btn-outline-danger" type="button">Ver</button>
+            </div>
+          </div>
+        </div>
+        @endforeach
+        
+      </div>
+    </div>
+  </div>
 
 @endsection
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,10 @@ class ComposerAutoloaderInit82318e82c3c3f8cef787c4e6a7a5f805
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit82318e82c3c3f8cef787c4e6a7a5f805', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit82318e82c3c3f8cef787c4e6a7a5f805', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
