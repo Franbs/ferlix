4be372cb96b65ebf22a4b4a2322ec462cf5c8e89
EduBS <2wiawedubs@gmail.com>
2022-01-25 16:25:44 +0100

    Fixed problem with there isn't favorites

--- resources/views/user/stream.blade.php
+++ resources/views/user/stream.blade.php
@@ -19,7 +19,7 @@
         {{-- {{ dd(app("request")->session()->get("favoriteList")) }} --}}
         
         <div class="col-1">
-            @if (in_array($movieID, app("request")->session()->get("favoriteList")))
+            @if (in_array($movieID, app("request")->session()->get("favoriteList", [])))
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="yellow" stroke="black"  viewBox="0 0 24 24" class="bi bi-star float-end">
                     <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
                 </svg>
