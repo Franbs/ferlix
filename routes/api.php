95458e71925283e46dee2d0131cd29e79497b1af
Franbs <12francescbs@gmail.com>
2022-03-01 17:05:49 +0100

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -35,24 +35,52 @@ Route::get('/billboard', function () {
 
 Route::post('/movie', [MovieController::class, 'store']);
 
-Route::get('/stream/{id}', [MovieController::class, 'viewWithId']);
-
 Route::put('/updateMovie', [MovieController::class, 'updateMovie']);
 
 Route::get('/movieDel/{id}', [MovieController::class, 'deleteMovie']);
 
-/*Route::middleware('auth:sanctum')->post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
- 
-    return ['token' => $token->plainTextToken];
-});*/
+Route::middleware(["auth:sanctum"])->group(function () {
+    Route::get('/stream/{id}', [MovieController::class, 'viewWithId']);
+});
 
-//Protecting Routes
-/*Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/profile', function(Request $request) {
-        return auth()->user();
+
+/*
+Route::post("/login", function (Request $request) {
+    $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+        'remember_me' => 'boolean'
+    ]);
+
+    $credentials = request(['email', 'password']);
+    if (!Auth::attempt($credentials))
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
+    else {
+        $userRol = $request->user()["rol"];
+        $token = $request->user()->createToken("token-" . $request["email"], [strval($userRol)]);
+
+        return response()->json(
+            ["token" => $token->plainTextToken],
+            200
+        );
+    }
+});
+
+Route::middleware(["auth:sanctum"])->group(function () {
+    Route::get("/movies", function () {
+        return new MovieCollection(Movie::all());
     });
+    Route::get("/movies/{id}", function ($id) {
+        return new MovieResource(Movie::find($id));
+    });
+    Route::get('/stream/{id}', [MovieController::class, 'viewWithId']);
+});
 
-    // API route for logout user
-    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
-});*/
+Route::middleware(["auth:sanctum", "abilities:admin"])->group(function () {
+    Route::post("/movies", [MovieController::class, "store"]);
+    Route::put("/movies/{id}", [MovieController::class, "update"]);
+    Route::delete("/movies/{id}", [MovieController::class, "destroy"]);
+});
+*/
